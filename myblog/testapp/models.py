8ef9a0d5a475dc032378b6f7fde2928654a11256
John Doe <johndoe@example.com>
2021-01-17 18:39:08 +0100

    kraj 4 predavanja

---
+++ myblog/testapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+#fajl u kome pisemo svoje modele
+#definisemo promjenjivu i koji je tip podatka ovo name i CharField
+
+
+class Dog(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    
+    def __str__(self):
+        return self.name +', '+ str(self.age)
+
+
